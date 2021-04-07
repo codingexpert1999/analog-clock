4d46e128b33fc5c40f0c8a4d3806a2b76ba66530
Michalis Michailidis <mixalismixailidis857@gmail.com>
2021-04-08 00:06:53 +0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const hoursPointer = document.getElementById("hours")
+const minutesPointer = document.getElementById("minutes");
+const secondsPointer = document.getElementById("seconds");
+
+const today = new Date()
+
+let hours = today.getHours();
+let minutes = today.getMinutes();
+let seconds = today.getSeconds();
+
+let time = setInterval(() => {
+    seconds++;
+
+    if(seconds === 60){
+        seconds = 0;
+        minutes++;
+
+        if(minutes === 60){
+            minutes = 0;
+
+            hours++;
+
+            if(hours === 12){
+                hours = 0;
+                minutes = 0;
+                seconds = 0;
+            }
+        }
+    }
+
+    hoursPointer.style.transform = `rotateZ(${(hours * 30) + (minutes * 6 * 60 * 12)}deg)`
+    minutesPointer.style.transform = `rotateZ(${(minutes * 6) + (seconds * 6 * 60)}deg)`
+    secondsPointer.style.transform = `rotateZ(${seconds * 6}deg)`
+}, 1000)
