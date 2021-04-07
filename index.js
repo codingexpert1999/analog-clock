6fe718ad94525b058ecf7ffebecd0d07b5127f32
Michalis Michailidis <mixalismixailidis857@gmail.com>
2021-04-08 00:23:37 +0300

    Final Commit

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ let hours = today.getHours();
 let minutes = today.getMinutes();
 let seconds = today.getSeconds();
 
+hoursPointer.style.transform = `rotateZ(${(hours * 30) + (minutes * 6 * 60 * 12)}deg)`
+minutesPointer.style.transform = `rotateZ(${(minutes * 6) + (seconds * 6 * 60)}deg)`
+secondsPointer.style.transform = `rotateZ(${seconds * 6}deg)`
+
 let time = setInterval(() => {
     seconds++;
 
